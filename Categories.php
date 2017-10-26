0d1014f7abd066f91feaa06b7cd1d9e0dae0fdfc
swap22 <sharma.sswapnil@gmail.com>
2017-10-27 03:31:16 +0530

    basic structure is created for the categories page

---
+++ Categories.php
@@ -0,0 +1,143 @@
+<!DOCTYPE>
+
+<html>
+	<head>
+		<title>Manage Categories</title>
+		 <link rel="stylesheet" href="vendors/css/bootstrap.min.css">
+		 <link rel="stylesheet" href="resources/css/adminstyles.css">
+<style>
+	.FieldInfo{
+    color: rgb(251, 174, 44);
+    font-family: Bitter,Georgia,"Times New Roman",Times,serif;
+    font-size: 1.2em;
+}
+
+</style>
+                
+	</head>
+	<body>
+		<div style="height: 10px; background: #27aae1;"></div>
+<nav class="navbar navbar-inverse" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+		data-target="#collapse">
+		<span class="sr-only">Toggle Navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	</button>
+	<a class="navbar-brand" href="Blog.php">
+	   <img style="margin-top: -12px;" src="resources/images/1.jpg" width=100;height=20;>
+	</a>
+		</div>
+		<div class="collapse navbar-collapse" id="collapse">
+		<ul class="nav navbar-nav">
+			<li><a href="#">Home</a></li>
+			<li class="active"><a href="Blog.php" target="_blank">Blog</a></li>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">Feature</a></li>
+		</ul>
+		<form action="Blog.php" class="navbar-form navbar-right">
+		<div class="form-group">
+		<input type="text" class="form-control" placeholder="Search" name="Search" >
+		</div>
+	         <button class="btn btn-default" name="SearchButton">Go</button>
+			
+		</form>
+		</div>
+		
+	</div>
+</nav>
+<div class="Line" style="height: 10px; background: #27aae1;"></div>
+<div class="container-fluid">
+<div class="row">
+	
+	<div class="col-sm-2">
+	<br><br>
+	<ul id="Side_Menu" class="nav nav-pills nav-stacked">
+	<li ><a href="Dashboard.php">
+	<span class="glyphicon glyphicon-th"></span>
+	&nbsp;Dashboard</a></li>
+	<li><a href="AddNewPost.php">
+	<span class="glyphicon glyphicon-list-alt"></span>
+	&nbsp;Add New Post</a></li>
+	<li class="active"><a href="Categories.php">
+	<span class="glyphicon glyphicon-tags"></span>
+	&nbsp;Categories</a></li>
+	<li><a href="Admins.php">
+	<span class="glyphicon glyphicon-user"></span>
+	&nbsp;Manage Admins</a></li>
+	<li ><a href="Comments.php">
+	<span class="glyphicon glyphicon-comment"></span>
+	&nbsp;Comments</a></li>
+	<li><a href="Blog.php?Page=1" target="_Blank">
+	<span class="glyphicon glyphicon-equalizer"></span>
+	&nbsp;Live Blog</a></li>
+	<li><a href="Logout.php">
+	<span class="glyphicon glyphicon-log-out"></span>
+	&nbsp;Logout</a></li>	
+		
+	</ul>
+	
+	
+	
+	
+	</div> <!-- Ending of Side area -->
+	<div class="col-sm-10">
+	<h1>Manage Categories</h1>
+	
+<div>
+<form action="Categories.php" method="post">
+	<fieldset>
+	<div class="form-group">
+	<label for="categoryname"><span class="FieldInfo">Name:</span></label>
+	<input class="form-control" type="text" name="Category" id="categoryname" placeholder="Name">
+	</div>
+	<br>
+<input class="btn btn-success btn-block" type="Submit" name="Submit" value="Add New Category">
+	</fieldset>
+	<br>
+</form>
+</div>
+<div class="table-responsive">
+	<table class="table table-striped table-hover">
+	<tr>
+		<th>Sr No.</th>
+		<th>Date & Time</th>
+		<th>Category Name</th>
+		<th>Creator Name</th>
+		<th>Action</th>
+		
+	</tr>
+<tr>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td>
+	<span class="btn btn-danger">Delete</span>
+	</a></td>
+	
+</tr>
+		
+		
+	</table>
+</div>
+	</div> <!-- Ending of Main Area-->
+	
+</div> <!-- Ending of Row-->
+	
+</div> <!-- Ending of Container-->
+		<div id="Footer">
+			<hr><p>  Swapnil Sharma &copy;2017-2020 --- All right reserved.
+			</p>	
+		</div>
+		<div style="height: 10px; background: #27AAE1;"></div>
+		<script src="vendors/js/jquery.min.js"></script>
+    	<script src="vendors/js/bootstrap.min.js"></script>
+	    
+	</body>
+</html>
