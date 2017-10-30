a64a6a0f6f7a85897ecc3da861a21b279c8fbb46
swap22 <sharma.sswapnil@gmail.com>
2017-10-30 21:06:02 +0530

    created frontend of the blog

---
+++ Blog.php
@@ -0,0 +1,192 @@
+<?php require_once("Include/db.php"); ?>
+<?php require_once("Include/Sessions.php"); ?>
+<?php require_once("Include/Functions.php"); ?>
+<!DOCTYPE>
+
+<html>
+	<head>
+		<title>Main Page</title>
+    	 <link rel="stylesheet" href="vendors/css/bootstrap.min.css">
+		 <link rel="stylesheet" href="resources/css/frontend.css">
+                    <style>
+		
+
+nav ul li{
+    float: left;
+}
+
+	       </style> 
+	</head>
+	<body>
+<div style="height: 10px; background: #27aae1;"></div>
+<nav class="navbar navbar-inverse" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+		data-target="#collapse">
+		<span class="sr-only">Toggle Navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	</button>
+	<a class="navbar-brand" href="Blog.php">
+	   <img style="margin-top: -12px;" src="resources/images/1.jpg" width=100;height=20;>
+	</a>
+		</div>
+		<div class="collapse navbar-collapse" id="collapse">
+		<ul class="nav navbar-nav">
+			<li><a href="#">Home</a></li>
+			<li class="active"><a href="Blog.php">Blog</a></li>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">Feature</a></li>
+		</ul>
+		<form action="Blog.php" class="navbar-form navbar-right">
+		<div class="form-group">
+		<input type="text" class="form-control" placeholder="Search" name="Search" >
+		</div>
+	         <button class="btn btn-default" name="SearchButton">Go</button>
+			
+		</form>
+		</div>
+		
+	</div>
+</nav>
+<div class="Line" style="height: 10px; background: #27aae1;"></div>
+<div class="container"> <!--Container-->
+	<div class="blog-header">
+	<h1>THis is heading of the post</h1>
+	<p class="lead">This is Some heading to write about author</p>
+	</div>
+	<div class="row"> <!--Row-->
+		<div class="col-sm-8"> <!--Main Blog Area-->
+		<?php
+		global $ConnectingDB;
+	
+			
+		$ViewQuery="SELECT * FROM admin_panel ORDER BY id desc LIMIT 0,3";
+		$Execute=mysql_query($ViewQuery);
+		while($DataRows=mysql_fetch_array($Execute)){
+			$PostId=$DataRows["id"];
+			$DateTime=$DataRows["datetime"];
+			$Title=$DataRows["title"];
+			$Category=$DataRows["category"];
+			$Admin=$DataRows["author"];
+			$Image=$DataRows["image"];
+			$Post=$DataRows["post"];
+		
+		?>
+		<div class="blogpost thumbnail">
+			<img class="img-responsive img-rounded"src="Upload/<?php echo $Image;  ?>" >
+		<div class="caption">
+			<h1 id="heading"> <?php echo htmlentities($Title); ?></h1>
+		<p class="description">Category:<?php echo htmlentities($Category); ?> Published on
+		<?php echo htmlentities($DateTime);?>		
+		</p>
+		<p class="post"><?php
+		if(strlen($Post)>150){$Post=substr($Post,0,150).'...';}
+		
+		echo $Post; ?></p>
+		</div>
+		<a href="FullPost.php?id=<?php echo $PostId; ?>"><span class="btn btn-info">
+			Read More &rsaquo;&rsaquo;
+		</span></a>
+			
+		</div>
+		<?php } ?>
+		
+		</div> <!--Main Blog Area Ending-->
+		<div class="col-sm-offset-1 col-sm-3"> <!--Side Area -->
+			<h2>About me </h2>
+	<img class=" img-responsive img-circle imageicon" src="images/Bunny.jpg">		
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		, sed do eiusmod tempor incididunt ut labore et dolore magna
+		aliqua. Ut enim ad minim veniam, quis nostrud exercitation ul
+		lamco laboris nisi ut aliquip ex ea commodo consequat. Duis a
+		ute irure dolor in reprehenderit in voluptate velit esse cill
+		um dolore eu fugiat nulla pariatur. Excepteur sint occaecat c
+		upidatat non proi
+		dent, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<div class="panel panel-primary">
+	<div class="panel-heading">
+		<h2 class="panel-title">Categories</h2>
+	</div>
+	<div class="panel-body">
+<?php
+global $ConnectingDB;
+$ViewQuery="SELECT * FROM category ORDER BY id desc";
+$Execute=mysql_query($ViewQuery);
+while($DataRows=mysql_fetch_array($Execute)){
+	$Id=$DataRows['id'];
+	$Category=$DataRows['name'];
+?>
+<a href="Blog.php?Category=<?php echo $Category; ?>">
+<span id="heading"><?php echo $Category."<br>"; ?></span>
+</a>
+<?php } ?>
+		
+	</div>
+	<div class="panel-footer">
+		
+		
+	</div>
+</div>
+
+
+
+
+<div class="panel panel-primary">
+	<div class="panel-heading">
+		<h2 class="panel-title">Recent Posts</h2>
+	</div>
+	<div class="panel-body background">
+<?php
+$ConnectingDB;
+$ViewQuery="SELECT * FROM admin_panel ORDER BY id desc LIMIT 0,5";
+$Execute=mysql_query($ViewQuery);
+while($DataRows=mysql_fetch_array($Execute)){
+	$Id=$DataRows["id"];
+	$Title=$DataRows["title"];
+	$DateTime=$DataRows["datetime"];
+	$Image=$DataRows["image"];
+	if(strlen($DateTime)>11){$DateTime=substr($DateTime,0,12);}
+	?>
+<div>
+<img class="pull-left" style="margin-top: 10px; margin-left: 0px;"  src="Upload/<?php echo htmlentities($Image); ?>" width=120; height=60;>
+    <a href="FullPost.php?id=<?php echo $Id;?>">
+     <p id="heading" style="margin-left: 130px; padding-top: 10px;"><?php echo htmlentities($Title); ?></p>
+     </a>
+     <p class="description" style="margin-left: 130px;"><?php echo htmlentities($DateTime);?></p>
+	<hr>
+</div>	
+	
+	
+	
+<?php } ?>		
+		
+	</div>
+	<div class="panel-footer">
+		
+		
+	</div>
+</div>
+		
+		
+		
+		
+		</div> <!--Side Area Ending-->
+	</div> <!--Row Ending-->
+	
+	
+</div><!--Container Ending-->
+	<div id="Footer">
+			<hr><p>  Swapnil Sharma &copy;2017-2020 --- All right reserved.
+			</p>	
+		</div>
+		<div style="height: 10px; background: #27AAE1;"></div>
+		<script src="vendors/js/jquery.min.js"></script>
+    	<script src="vendors/js/bootstrap.min.js"></script>
+	    
+	</body>
+</html>
